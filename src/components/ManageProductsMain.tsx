1b5c257f59a9a7222793a9295edd2bf2f58b37dd
ImAdityaa12 <adityagupta1291@gmail.com>
2024-11-23 00:48:31 +0530

    Added 2 card in row layout

--- src/components/ManageProductsMain.tsx
+++ src/components/ManageProductsMain.tsx
@@ -11,7 +11,7 @@ const ManageProduct = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-6 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-6 gap-4 max-sm:grid-cols-2">
       {products.map((product) => (
         <ProductCard
           key={product.product._id}
