a8d3ed43267aa52b33385b64ec115796453ca9c9
ImAdityaa12 <adityagupta1291@gmail.com>
2024-11-25 22:18:49 +0530

    Added image in category and model

--- src/app/(main)/product-tags/page.tsx
+++ src/app/(main)/product-tags/page.tsx
@@ -3,6 +3,8 @@ import { ContentLayout } from "@/components/admin-panel/content-layout";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import MultipleSelector, { Option } from "@/components/ui/multiple-selector";
 import {
   ResponsiveModal,
@@ -13,6 +15,7 @@ import {
 } from "@/components/ui/responsive-dialog";
 import { getCookie } from "@/lib/utils";
 import { Plus, X } from "lucide-react";
+import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import { toast } from "sonner";
 
@@ -21,6 +24,8 @@ const ProductTagsPage = () => {
   const [selectedCategory, setSelectedCategory] = useState<
     "brands" | "models" | "categories" | null
   >(null);
+  const [imagePreview, setImagePreview] = useState<string | null>(null);
+  const [brands, setBrands] = useState<Option[]>([]);
   const [data, setData] = useState<{
     brands: string[];
     categories: string[];
@@ -30,11 +35,50 @@ const ProductTagsPage = () => {
     categories: [],
     models: [],
   });
-  // const data = {
-  //   brands: ["havells"],
-  //   categories: ["shoes", "shoes1", "shoes2", "shoes3", "shoes4", "shoes5"],
-  //   models: ["coral", "coral1"],
-  // };
+  const [image, setImage] = useState<string | null>(null);
+  const handleImageUpload = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      // Create a preview
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImagePreview(reader.result as string);
+      };
+      reader.readAsDataURL(file);
+
+      // Upload to server
+      const formData = new FormData();
+      formData.append("image", file);
+
+      const message = toast.loading("Uploading image...");
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}upload/image`,
+          {
+            method: "POST",
+            body: formData,
+          }
+        );
+
+        if (!response.ok) {
+          toast.error("Error uploading image", { id: message, duration: 3000 });
+          throw new Error("Image upload failed");
+        }
+
+        const data = await response.json();
+        setImage(data?.result?.url);
+        toast.success("Image uploaded successfully", {
+          id: message,
+          duration: 3000,
+        });
+      } catch (error) {
+        console.error("Error uploading image:", error);
+        toast.error("Error uploading image", { id: message, duration: 3000 });
+      }
+    }
+  };
   const getTags = async () => {
     try {
       const res = await fetch(
@@ -56,7 +100,7 @@ const ProductTagsPage = () => {
   useEffect(() => {
     getTags();
   }, []);
-  const [value, setValue] = useState<Option[]>([]);
+  const [value, setValue] = useState<string>("");
   const handleSendRequest = async () => {
     try {
       if (selectedCategory === "models") {
@@ -68,7 +112,7 @@ const ProductTagsPage = () => {
               "Content-Type": "application/json",
               Authorization: `Bearer ${getCookie("token")}`,
             },
-            body: JSON.stringify({ models: value.map((item) => item.value) }),
+            body: JSON.stringify({ model: value, image: image }),
           }
         );
         if (res.status === 200) {
@@ -77,7 +121,8 @@ const ProductTagsPage = () => {
         } else {
           toast.error("Error adding models");
         }
-      } else if (selectedCategory === "categories") {
+      }
+      if (selectedCategory === "categories") {
         const res = await fetch(
           `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}admin/products/addCategory`,
           {
@@ -87,15 +132,17 @@ const ProductTagsPage = () => {
               Authorization: `Bearer ${getCookie("token")}`,
             },
             body: JSON.stringify({
-              categories: value.map((item) => item.value),
+              category: value,
+              image: image,
             }),
           }
         );
-        if (res.status === 200) {
-          toast.success("Categories added successfully");
+        const data = await res.json();
+        if (res.status === 201) {
+          toast.success("Category added successfully");
           setIsModalOpen(false);
         } else {
-          toast.error("Error adding categories");
+          toast.error(data.message);
         }
       }
       if (selectedCategory === "brands") {
@@ -107,7 +154,7 @@ const ProductTagsPage = () => {
               "Content-Type": "application/json",
               Authorization: `Bearer ${getCookie("token")}`,
             },
-            body: JSON.stringify({ brands: value.map((item) => item.value) }),
+            body: JSON.stringify({ brands: brands.map((item) => item.value) }),
           }
         );
         if (res.status === 200) {
@@ -117,7 +164,9 @@ const ProductTagsPage = () => {
           toast.error("Error adding brands");
         }
       }
-      setValue([]);
+      setValue("");
+      setBrands([]);
+      setImage(null);
       getTags();
     } catch (error) {
       console.log(error);
@@ -150,21 +199,48 @@ const ProductTagsPage = () => {
                 : ""}
             </ResponsiveModalTitle>
           </ResponsiveModalHeader>
-          <MultipleSelector
-            selectFirstItem={false}
-            value={value}
-            creatable
-            onChange={(values) => {
-              setValue(values);
-              console.log(value);
-            }}
-            placeholder="Select frameworks you like..."
-            emptyIndicator={
-              <p className="text-center text-lg leading-10 text-gray-600 dark:text-gray-400">
-                no results found.
-              </p>
-            }
-          />
+          {selectedCategory === "models" ||
+          selectedCategory === "categories" ? (
+            <div>
+              <Label htmlFor="image">Product Image</Label>
+              <Input
+                id="image"
+                type="file"
+                accept="image/*"
+                onChange={handleImageUpload}
+                required
+              />
+              {imagePreview && (
+                <Image
+                  src={imagePreview}
+                  alt="Preview"
+                  width={200}
+                  height={200}
+                  className="mt-2 max-w-xs h-auto"
+                />
+              )}
+              <Input
+                onChange={(e) => {
+                  setValue(e.target.value);
+                }}
+              />
+            </div>
+          ) : (
+            <MultipleSelector
+              selectFirstItem={false}
+              value={brands}
+              creatable
+              onChange={(values) => {
+                setBrands(values);
+              }}
+              placeholder="Select frameworks you like..."
+              emptyIndicator={
+                <p className="text-center text-lg leading-10 text-gray-600 dark:text-gray-400">
+                  no results found.
+                </p>
+              }
+            />
+          )}
           <ResponsiveModalFooter className="mt-4">
             <Button className="btn btn-primary" onClick={handleSendRequest}>
               Save
