a97f634453345c255a38c48973d808337aaff310
ImAdityaa12 <adityagupta1291@gmail.com>
2025-01-11 23:04:12 +0530

    Remove unused product quantity discounts page component

--- src/app/(main)/product-quanity-discounts/page.tsx
+++
@@ -1,8 +0,0 @@
-import { ContentLayout } from "@/components/admin-panel/content-layout";
-import React from "react";
-
-const page = () => {
-  return <ContentLayout title="Discounts">page</ContentLayout>;
-};
-
-export default page;
