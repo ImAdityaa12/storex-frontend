f5dd3743515cffb5efeb369163c59fcb872f617d
ImAdityaa12 <adityagupta1291@gmail.com>
2025-09-20 19:46:05 +0530

    feat: Implement login form component and update login page
    This commit introduces a new login form component and integrates it into the login page.
    
    The login form component includes:
    
    - UI elements for phone/email and password input.
    - Functionality for handling login submissions.
    - Integration with backend API for user authentication.
    - Remember me function
    - Social Login
    - UI Enhancements
    
    The login page has been updated to utilize the new login form component.
    Middleware has been updated to allow public assets.

--- src/middleware.ts
+++ src/middleware.ts
@@ -40,7 +40,8 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - public assets (images, fonts, etc.)
      */
-    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:png|jpg|jpeg|gif|webp|svg|ico|woff|woff2|ttf|eot)).*)",
   ],
 };
